59495c674aecf8b5ceaded61dbb1836af611058f
Eedoh <ekabelly@gmail.com>
2021-10-21 19:29:47 +0300

    first commit

---
+++ src/layout/map-layout.js
@@ -0,0 +1,11 @@
+import '.assets/style/map-layout.scss';
+
+function MapLayout() {
+    return (
+        <div className="map-layout">
+            <h1>Polygon Map</h1>
+        </div>
+    );
+}
+
+export default MapLayout;
